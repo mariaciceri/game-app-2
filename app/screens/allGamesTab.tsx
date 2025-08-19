99b14f54589c5d82faff2d3c87ff99187c811ba9
Maria <mariafacchin00@hotmail.com>
2025-08-19 15:35:19 +0200

    Improve tab to display all games

--- app/screens/allGamesTab.tsx
+++ app/screens/allGamesTab.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import AllGameList from '@/components/AllGameList';
+import useGames from '@/hooks/useGames';
 
 
 
 export default function ProfileScreen() {
-    
+    const { games } = useGames();
   
     return (
-        <SafeAreaView>
-            <Text style={{fontSize: 20, textAlign: 'center', marginTop: 20 }}>
-                All Games Screen
+        <SafeAreaView style={{flex: 1, backgroundColor: "black"}}>
+            <Text style={{fontSize: 20, textAlign: 'center', color: "white" }}>
+                You can find (almost) all of your games here!
             </Text>
-            
+            <AllGameList games={games} />
+
         </SafeAreaView>
     );
 }

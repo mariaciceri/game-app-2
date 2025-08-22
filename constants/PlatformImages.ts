f62ecee803c06e0435ec1dcb315707f2386995d1
Maria <mariafacchin00@hotmail.com>
2025-08-22 13:57:39 +0200

    Extract platform constants

---
+++ constants/PlatformImages.ts
@@ -0,0 +1,7 @@
+import { Platform } from '@/types/Platforms';
+
+export const PlatformImages: Record<Platform, any> = {
+    Steam: require('@/assets/images/steam.webp'),
+    XBox: require('@/assets/images/xbox.webp'),
+    PS: require('@/assets/images/playstation.webp')
+};

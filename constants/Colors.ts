1cba025f9718bc3bdb05bf4776183d70495a36fd
Maria <mariafacchin00@hotmail.com>
2025-08-22 13:57:27 +0200

    Update colors in constant folder

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,27 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const primary = 'darkturquoise';
+const secondary = '#9356a3';
 
-export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+export const Colors = {
+  primary,
+  secondary
 };
+
+// example:
+// export default {
+// light: {
+//   text: '#000',
+//     background: '#fff',
+//       tint: tintColorLight,
+//         tabIconDefault: '#ccc',
+//           tabIconSelected: tintColorLight,
+//   },
+// dark: {
+//   text: '#fff',
+//     background: '#000',
+//       tint: tintColorDark,
+//         tabIconDefault: '#ccc',
+//           tabIconSelected: tintColorDark,
+//   },
+// };
+
+ 

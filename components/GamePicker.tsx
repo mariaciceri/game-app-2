394b8bc3a0d6b23b3d126b711a32bda7ff73859c
Maria <mariafacchin00@hotmail.com>
2025-08-30 19:19:50 +0200

    Update color scheme

--- components/GamePicker.tsx
+++ components/GamePicker.tsx
@@ -15,13 +15,13 @@ export default function GamePicker({ gamePlatform, onSelect } : GamePickerProps)
             style={ styles.picker }
             onValueChange={(itemValue) => onSelect(itemValue)}
         >
-            <Picker.Item label="Select a game" value="" color='white' />
-            <Picker.Item label="PlayStation" value="PS" color='white' />
-            <Picker.Item label="XBox" value="XBox" color='white' />
-            <Picker.Item label="Nintendo" value="Nintendo" color='white' />
-            <Picker.Item label="Game 4" value="Game 4" color='white' />
-            <Picker.Item label="Game 5" value="Game 5" color='white' />
-            <Picker.Item label="Game 6" value="Game 6" color='white' />
+            <Picker.Item label="Select a game" value="" color={Colors.dark} />
+            <Picker.Item label="PlayStation" value="PS" color={Colors.dark} />
+            <Picker.Item label="XBox" value="XBox" color={Colors.dark} />
+            <Picker.Item label="Nintendo" value="Nintendo" color={Colors.dark} />
+            <Picker.Item label="Game 4" value="Game 4" color={Colors.dark} />
+            <Picker.Item label="Game 5" value="Game 5" color={Colors.dark} />
+            <Picker.Item label="Game 6" value="Game 6" color={Colors.dark} />
         </Picker>
     )
 }
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
     picker: {
         height: '30%',
         width: '90%',
-        backgroundColor: Colors.light,
+        backgroundColor: "white",
         marginTop: 20
     }
 })
